a8bcb0deb45345fcca9e1ab9aa70865a6dc2cb93
WHOOPIE WANJA <48823916+woupyie@users.noreply.github.com>
2020-07-21 03:27:38 +0300

    Admin can add product features.

--- app/src/main/java/com/woupyie/thrifty/AdminAddNewProductActivity.java
+++ app/src/main/java/com/woupyie/thrifty/AdminAddNewProductActivity.java
@@ -1,14 +1,29 @@
 package com.woupyie.thrifty;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
-    private  String CategoryName;
+    private  String CategoryName, Description, Price, Pname, saveCurrentDate, saveCurrentTime;
+    private Button AddNewProductButton;
+    private ImageView InputProductImage;
+    private EditText InputProductName, InputProductDescription, InputProductPrice;
+    private  static  final int GalleryPick = 1;
+    private Uri ImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +32,76 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
 
         CategoryName = getIntent().getExtras().get("category").toString();
 
-        Toast.makeText(this, CategoryName,Toast.LENGTH_SHORT).show();
+        AddNewProductButton = findViewById(R.id.add_new_product);
+        InputProductImage = findViewById(R.id.select_product_image);
+        InputProductName = findViewById(R.id.product_name);
+        InputProductDescription = findViewById(R.id.product_description);
+        InputProductPrice = findViewById(R.id.product_price);
 
+        InputProductImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenGallery();
+
+            }
+        });
+
+        AddNewProductButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ValidateProductData();
+            }
+        });
+
+    }
+
+    private void ValidateProductData() {
+        Description = InputProductDescription.getText().toString();
+        Price = InputProductPrice.getText().toString();
+        Pname = InputProductName.getText().toString();
+
+        if (ImageUri == null ){
+            Toast.makeText(this, "Product Image is required.", Toast.LENGTH_SHORT).show();
+
+        } else if (TextUtils.isEmpty(Description)){
+            Toast.makeText(this, "Please write product description.", Toast.LENGTH_SHORT).show();
+
+        }else if (TextUtils.isEmpty(Price)){
+            Toast.makeText(this, "Please write product price.", Toast.LENGTH_SHORT).show();
+
+        }else if (TextUtils.isEmpty(Pname)){
+            Toast.makeText(this, "Please write product name.", Toast.LENGTH_SHORT).show();
+
+        }else{
+            StoreProductInformation();
+        }
+
+    }
+
+    private void StoreProductInformation() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("DD, MM, YYYY");
+        saveCurrentDate = currentDate.format(calendar.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentTime.format(calendar.getTime());
+    }
+
+    private void OpenGallery() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GalleryPick);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GalleryPick && resultCode==RESULT_OK && data != null){
+            ImageUri = data.getData();
+            InputProductImage.setImageURI(ImageUri);
+;
+        }
     }
 }
