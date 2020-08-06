96e7e10b03d58f2d69a0e283664cc7f97b3e64f6
WHOOPIE WANJA <48823916+woupyie@users.noreply.github.com>
2020-08-06 17:32:48 +0300

    validations

--- app/src/main/java/com/woupyie/thrifty/AdminCategoryActivity.java
+++ app/src/main/java/com/woupyie/thrifty/AdminCategoryActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class AdminCategoryActivity extends AppCompatActivity {
@@ -13,10 +14,35 @@ public class AdminCategoryActivity extends AppCompatActivity {
     private ImageView glasses, hatsCaps, walletsBagsPurses,shoes ;
     private ImageView headPhonesHandsFree, laptops, watches, mobilePhones;
 
+    private Button logoutBtn, checkOrdersBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_category);
+
+        logoutBtn = findViewById(R.id.admin_logout_btn);
+        checkOrdersBtn = findViewById(R.id.check_orders_btn);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        checkOrdersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminNewOrdersActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
         tShirts = findViewById(R.id.t_shirts);
         sportsTshirts = findViewById(R.id.sports_t_shirts);
