4fb21c63adf182dc315c4e7ea46d0ca021ed1b86
WHOOPIE WANJA <48823916+woupyie@users.noreply.github.com>
2020-08-07 03:15:31 +0300

    admin can remove the already shipped orders

--- app/src/main/java/com/woupyie/thrifty/AdminNewOrdersActivity.java
+++ app/src/main/java/com/woupyie/thrifty/AdminNewOrdersActivity.java
@@ -1,11 +1,14 @@
 package com.woupyie.thrifty;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -66,6 +69,43 @@ public class AdminNewOrdersActivity extends AppCompatActivity {
                             }
                         });
 
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                CharSequence options [] = new CharSequence[] {
+                                        "Yes",
+                                        "No"
+
+                                };
+
+                                AlertDialog.Builder builder = new AlertDialog.Builder(AdminNewOrdersActivity.this);
+                                builder.setTitle("Have you shipped this order ?");
+
+                                builder.setItems(options, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                        if(which == 0){
+                                            String uID = getRef(position).getKey();
+                                            RemoveOrder(uID);
+
+                                        }
+                                        else {
+                                            finish();
+
+                                        }
+
+                                    }
+                                });
+
+                                builder.show();
+
+
+                            }
+                        });
+
+
+
                     }
 
                     @NonNull
@@ -79,6 +119,8 @@ public class AdminNewOrdersActivity extends AppCompatActivity {
         ordersList.setAdapter(adapter);
         adapter.startListening();
     }
+
+
 
     public static class AdminOrdersViewHolder extends  RecyclerView.ViewHolder{
 
@@ -96,6 +138,11 @@ public class AdminNewOrdersActivity extends AppCompatActivity {
 
             showOrdersButton = itemView.findViewById(R.id.show_all_products_btn);
         }
+    }
+
+
+    private void RemoveOrder(String uID) {
+        ordersRef.child(uID).removeValue();
     }
 }
 
